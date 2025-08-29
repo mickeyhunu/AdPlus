2f64933903709f54a6420b49649380bae3c0f613
mickeyhunu <110079472+mickeyhunu@users.noreply.github.com>
2025-08-30 06:22:43 +0900

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -27,5 +27,5 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(frontBuild, "index.html"));
 });
 
-const PORT = Number(process.env.PORT || 4000);
+const PORT = Number(process.env.PORT || 80);
 app.listen(PORT, () => console.log(`Server listening on :${PORT}`));
